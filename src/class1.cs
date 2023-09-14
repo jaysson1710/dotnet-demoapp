ee6850d03ebdfe61c63739f78358bde54b4868b0
jaysson1710 <jaysson1710@hotmail.com>
2023-09-14 17:05:50 -0500

    Create class1.cs

---
+++ src/class1.cs
@@ -0,0 +1,10 @@
+namespace DotnetDemoapp
+{
+    public class class1
+    { 
+        public class1(int _numero){
+            numero =     _numero;
+        }
+        public int numero { get; set; }
+    }
+}
